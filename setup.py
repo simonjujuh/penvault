764426247b1b61a5c7b5b49191191f9fce83bd0f
simonjujuh <juhel.simon6@gmail.com>
2024-02-25 12:44:14 +0100

    Update deployment scripts

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'penvault=penvault.cli:main',  # Remplacez 'cli' par le nom de votre fichier principal
+            'penvault=penvault.cli:main',
         ],
     },
 )
