797108bf9e652d33f39f0eaf80e293e88946e02f
simonjujuh <juhel.simon6@gmail.com>
2024-04-19 10:45:50 +0200

    Add multiple vaults on open and close

--- penvault/cli.py
+++ penvault/cli.py
@@ -4,7 +4,11 @@ import sys
 import argparse
 from penvault.controller import VaultController
 
-import argcomplete
+try:
+    import argcomplete
+    completion = True
+except ImportError:
+    completion = False
 
 
 def main():
@@ -12,15 +16,23 @@ def main():
 
     # Parse command line
     parser = argparse.ArgumentParser(description='Pentest Vaults Manager')
+    parser.add_argument('-l', '--list', action='store_true', help='List vaults')
     parser.add_argument('-c', '--create', help='Specify the name for the new vault')
     parser.add_argument('-s', '--size', help='Specify the size for the new vault container')
-    parser.add_argument('-o', '--open', help='Specify the name of the vault to open').completer = app.complete_all_vaults
-    parser.add_argument('-C', '--close', help='Specify the name of the vault to close').completer = app.complete_opened_vaults
-    parser.add_argument('-l', '--list', action='store_true', help='List vaults')
-    parser.add_argument('--auto-mount', action='store_true', default=False, help='Automatically open the newly created container')
+    parser.add_argument('-a', '--auto-mount', action='store_true', default=False, help='Automatically open the newly created container')
+
+    if completion:
+        parser.add_argument('-o', '--open', metavar='VAULT', type=str, nargs='+', help='Specify the name of the vault to open').completer = app.complete_all_vaults
+        parser.add_argument('-x', '--close', metavar='VAULT', type=str, nargs='+', help='Specify the name of the vault to close').completer = app.complete_opened_vaults
+    else:
+        parser.add_argument('-o', '--open', help='Specify the name of the vault to open')
+        parser.add_argument('-x', '--close', help='Specify the name of the vault to close')
+
     parser.add_argument('--show-config', action='store_true', default=False, help='Automatically open the newly created container')
 
-    argcomplete.autocomplete(parser)
+    if completion: 
+        argcomplete.autocomplete(parser)
+    
     args = parser.parse_args()
 
     if args.create:
@@ -29,9 +41,11 @@ def main():
         else:
             app.create_vault(args.create, args.size, args.auto_mount)
     elif args.open:
-        app.open_vault(args.open)
+        for vault in args.open:
+            app.open_vault(vault)
     elif args.close:
-        app.close_vault(args.close)
+        for vault in args.close:
+            app.close_vault(vault)
     elif args.list:
         app.list_vaults()
     elif args.show_config:
