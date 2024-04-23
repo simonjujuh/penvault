07c06d732fc3fe211de34fc5893dfc23ec846000
simonjujuh <juhel.simon6@gmail.com>
2024-04-23 21:13:00 +0200

    Update list_mounted_containers returned structure

--- penvault/veracrypt.py
+++ penvault/veracrypt.py
@@ -39,6 +39,7 @@ def mount_container(container_path, mount_path, password):
     
     subprocess.run(command, text=True, check=True)
 
+
 def umount_container(container_path):
     command = [veracrypt_binary_path(), "--text", "--dismount", str(container_path)]
 
@@ -48,28 +49,12 @@ def umount_container(container_path):
 def list_mounted_containers():
     command = [veracrypt_binary_path(), "--text", "--list"]
     command_output = subprocess.run(command, text=True, capture_output=True)
-    return parse_veracrypt_output(command_output.stdout)
-    # return parse_veracrypt_output(run(command, capture_output=True))
 
+    # Parser les données
+    mounted_containers = []
+    for line in command_output.stdout.split('\n'):
+        if line.strip():
+            info = line.split(': ')
+            mounted_container.append(info[1].split())
 
-def parse_veracrypt_output(output):
-    entries = output.split('\n')
-    info_list = []
-
-    for entry in entries:
-        if entry.strip() == "":
-            continue
-        info = {}
-        parts = entry.split(':', 1)
-
-        if len(parts) == 2:
-            values = parts[1].split()
-            
-            info["path"] = values[0]
-            info["device"] = values[1]
-            info["mount point"] = values[2]
-
-            info_list.append(info)
-
-    return info_list
-
+    return mount_container
