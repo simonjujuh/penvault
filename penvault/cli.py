39cbaa15afc3a4784ec43a9fc88123a03d756456
simonjujuh <juhel.simon6@gmail.com>
2024-04-26 23:10:41 +0200

    Update completion functions

--- penvault/cli.py
+++ penvault/cli.py
@@ -3,17 +3,17 @@ import argparse
 from penvault.completion import *
 from penvault.config import mount_path
 
-
-try:
-    import argcomplete
-    completion = True
-except ImportError:
-    completion = False
-
+VERSION = "1.1"
 
 def build_cli_args():
-    parser = argparse.ArgumentParser(description='Pentest Vaults Manager')
 
+    try:
+        import argcomplete
+        completion = True
+    except ImportError:
+        completion = False
+    
+    parser = argparse.ArgumentParser(description=f'Pentest Vaults Manager {VERSION}')
     # List
     parser.add_argument('-l', '--list', action='store_true', help='List vaults')
     # Create
@@ -21,26 +21,25 @@ def build_cli_args():
     parser.add_argument('-s', '--size', help='Specify the size for the new vault container')
     parser.add_argument('-a', '--auto-mount', action='store_true', default=False, help='Automatically open the newly created container')
 
-    if completion:
-        
-        parser.add_argument('-o', '--open', metavar='VAULT', type=str, nargs='+', help='Specify the name of the vault to open').completer = complete_all_vaults
-        parser.add_argument('-x', '--close', metavar='VAULT', type=str, nargs='+', help='Specify the name of the vault to close').completer = complete_opened_vaults
+    if completion:  
+        parser.add_argument('-o', '--open', metavar='VAULT', type=str, nargs='+', help='Specify the name of the vault to open', choices=complete_all_vaults())
+        parser.add_argument('-x', '--close', metavar='VAULT', type=str, nargs='+', help='Specify the name of the vault to close', choices=complete_opened_vaults())
+        parser.add_argument('-r', '--resize', metavar='VAULT', type=str, nargs='+', help='Resize vault(s) to optimum space (WIP)', choices=complete_closed_vaults()
+)
+
     else:
         parser.add_argument('-o', '--open', metavar='VAULT', type=str, nargs='+', help='Open vault(s)')
         parser.add_argument('-x', '--close', metavar='VAULT', type=str, nargs='+', help='Close vault(s)')
-
-    # Resize vaults
-    parser.add_argument('-r', '--resize', metavar='VAULT', type=str, nargs='+', help='Resize vault(s) to optimum space')
+        parser.add_argument('-r', '--resize', metavar='VAULT', type=str, nargs='+', help='Resize vault(s) to optimum space (WIP)')
 
     # Check options
     parser.add_argument('--check-prune', action='store_true', default=False, help='Delete vaults older than a year')
     parser.add_argument('--check-cleanup', action='store_true', default=True, help=f'Check for residual directories in {mount_path} (default: True)')
 
-    # if completion: 
-    #     argcomplete.autocomplete(parser)
-    
-    args = parser.parse_args()
+    if completion: 
+        argcomplete.autocomplete(parser)
 
+    args = parser.parse_args()
 
     if len(sys.argv) == 1:
         parser.print_help()
--- penvault/completion.py
+++ penvault/completion.py
@@ -1,35 +1,39 @@
+from pathlib import Path
+from penvault import config
 from penvault.model import Vault, VaultsManager
 
+
 # Completion methods
-def complete_all_vaults(prefix, parsed_args, **kwargs):
+def complete_all_vaults():
     # List of containers from filesystem
     veracrypt_fs_containers = []
 
     # Get all .vc files from veracrypt container path and add it to the list
     for container in config.containers_path.glob("*.vc*"):
         vault = Vault('').from_container(container) # convert veracrypt to vault
-        veracrypt_fs_containers.append(vault)
+        veracrypt_fs_containers.append(vault.name)
 
-    # Build the completion tuple
-    complete_vaults = (vault for vault in veracrypt_fs_containers if vault.startswith(prefix))
-
-    # Return sorted tuple
-    return tuple(sorted(complete_vaults))
+    veracrypt_fs_containers.sort()
+    return veracrypt_fs_containers
 
 
-def complete_opened_vaults(prefix, parsed_args, **kwargs):
+def complete_opened_vaults():
     opened_vaults = []
     # Get the mounted containers
     mounted_containers = VaultsManager()._refresh_list(mounted_only=True)
-
+    
     # Build the liste of opened vaults
-    for vc_path in mounted_containers.keys:
+    for vc_path in mounted_containers.keys():
         vc_path = Path(vc_path)
         v = Vault('').from_container(vc_path)
         opened_vaults.append(v.name)
     
-    # Build the completion tuple
-    complete_vaults = (vault for vault in opened_vaults if vault.startswith(prefix))
-    
-    # Return sorted tuple
-    return tuple(sorted(complete_vaults))
+    opened_vaults.sort()
+    return opened_vaults
+
+
+def complete_closed_vaults():
+    all_vaults = complete_all_vaults()
+    opened_vaults = complete_opened_vaults()
+
+    return list(set(all_vaults) - set(opened_vaults))
