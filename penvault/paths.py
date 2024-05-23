e55301e3002f6f5bc150006d0f0e8d4aab660f8e
simonjujuh <juhel.simon6@gmail.com>
2024-05-23 21:33:02 +0200

    Update configs and paths

--- penvault/paths.py
+++ penvault/paths.py
@@ -1,5 +1,11 @@
+import os
 from pathlib import Path
 
-PENVAULT_PATH   = Path.home() / ".penvault"
-CONFIG_PATH     = PENVAULT_PATH / "config.ini"
-DATA_PATH       = Path(__file__).resolve().parent / "data"
+if os.getenv('PENVAULT_ENV') == 'test':
+    PENVAULT_PATH   = Path.home() / ".penvault"
+    CONFIG_PATH     = PENVAULT_PATH / "config_test.ini"
+    DATA_PATH       = Path(__file__).resolve().parent / "data"
+else:
+    PENVAULT_PATH   = Path.home() / ".penvault"
+    CONFIG_PATH     = PENVAULT_PATH / "config.ini"
+    DATA_PATH       = Path(__file__).resolve().parent / "data"
