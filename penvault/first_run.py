36e96b4d89f51ec6d98271d81bcfa66a26d8eae4
simonjujuh <juhel.simon6@gmail.com>
2024-07-14 17:43:11 +0200

    Capitalize some log messages

--- penvault/first_run.py
+++ penvault/first_run.py
@@ -7,7 +7,7 @@ from penvault.paths import CONFIG_PATH, PENVAULT_PATH, DATA_PATH
 def first_run_setup():
     # $HOME/.penvault/config.ini does not exist 
     if not CONFIG_PATH.exists():
-        log.info("first run detected, installing the configuration file")
+        log.info("First run detected, installing the configuration file")
         
         # $HOME/.penvault/ does not exist, create it
         if not PENVAULT_PATH.exists():
@@ -18,5 +18,5 @@ def first_run_setup():
         shutil.copyfile(template_file,CONFIG_PATH)
 
         # Inform end user to change config and exit
-        log.info(f"please edit {CONFIG_PATH} with the value of your choice...")
+        log.info(f"Please edit {CONFIG_PATH} with the value of your choice...")
         sys.exit(0)
