3947ff6ed1530989839fa2917c9a9c4b7ee52c7d
simonjujuh <juhel.simon6@gmail.com>
2024-07-15 18:53:07 +0200

    Add sorting to completion outputs

--- penvault/completion.py
+++ penvault/completion.py
@@ -7,13 +7,19 @@ containers_info = VaultsManager().get_containers_dict()
 # Completion methods
 # --archive
 def complete_all_vaults():
-    return [container for container in containers_info.keys()]
+    vaults = [container for container in containers_info.keys()]
+    vaults.sort()
+    return vaults
 
 # --close
 def complete_opened_vaults():
-    return [container_name for container_name, container_info in containers_info.items() if container_info is not None]
-    # return VaultsManager()._refresh_list(mounted_only=True)
+    vaults = [container_name for container_name, container_info in containers_info.items() if container_info is not None]
+    vaults.sort()
+    return vaults
 
 # --open
 def complete_closed_vaults():
-    return [container_name for container_name, container_info in containers_info.items() if container_info is None]
+    vaults = [container_name for container_name, container_info in containers_info.items() if container_info is None]
+    vaults.sort()
+    return vaults
+
